6e98f5b80147319f1449f54fc9fb86a1a1641166
Liamzkyyy <liamwebber.belarde@lorma.edu>
2025-04-05 01:20:54 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'screens/menu_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,50 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CoinFlip(),
-    );
-  }
-}
-
-class CoinFlip extends StatefulWidget {
-  const CoinFlip({super.key});
-
-  @override
-  _CoinFlipState createState() => _CoinFlipState();
-}
-
-class _CoinFlipState extends State<CoinFlip> {
-  int coinSide = 0;
-
-  void flipCoin() {
-    setState(() {
-      coinSide = Random().nextInt(2);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
-      appBar: AppBar(title: const Text("Coin Flip App")),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              coinSide == 0
-                  ? 'assets/images/heads.png'
-                  : 'assets/images/tails.png',
-              width: 150,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: flipCoin,
-              child: const Text("Flip Coin"),
-            ),
-          ],
-        ),
-      ),
+      title: 'Restaurant Menu',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const MenuListScreen(),
     );
   }
 }
